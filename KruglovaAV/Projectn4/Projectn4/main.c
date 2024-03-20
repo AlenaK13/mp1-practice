7c6d0ccfbc80824f2330ccb44cfc8065e4110428
AlenaK13 <kruglovatm@mail.ru>
2024-03-20 11:12:10 +0300

    Initial st

---
+++ KruglovaAV/Projectn4/Projectn4/main.c
@@ -0,0 +1,59 @@
+﻿#include "vector.h"
+
+int main(int arg, char** argv) {
+	int n, c;
+	int n1 = 0, n2 = 0, operation = 0;
+	TVector v1, v2, res;
+	char* infilename, * outfilename;
+
+	setlocale(LC_ALL, "ru");
+	if (arg < 4) {
+		printf("Некорректные аргументы\n");
+		return 1;
+	}
+	infilename = argv[2];
+	outfilename = argv[3];
+
+	/*printf("Lenght of v1 = \n");
+	scanf("%d", &n);
+	allocate(&v1, n);
+	fill(&v1);
+	print(&v1);*/
+	read(infilename, &v1, &v2);
+
+	while (1) {
+		printf("Выберите операцию над векторами: \n1- сложение векторов 2- вычитание векторов 3- скалярное произведение векторов 4- выход\n");
+		/*while ((c = getchar()) != '\n' && c != EOF) {}//очистка буфера ввода*/
+		//почему не воспользоваться int, без лишних проблем
+		scanf("%d", &operation);
+		if (operation == 1) { // есть же switch
+			res = sum(&v1, &v2);
+			write(outfilename, &res, "сумма");
+			//print(&res);
+		}
+		else if (operation == 2) {
+			res = sub(&v1, &v2);
+			write(outfilename, &res, "разность");
+		}
+		else if (operation == 3) {
+			res = prod(&v1, &v2);
+			write(outfilename, &res, "произведение");
+		}
+		else if (operation == 4) {
+			break;
+		}
+		else {
+			printf("Некорректный ввод\n");
+		}
+	}
+
+	free(v1.x);
+	free(v2.x);
+	if (res.x != NULL)
+	{
+		free(res.x);
+	}
+	return;
+}eturn ;
+}
+
---
+++ KruglovaAV/Projectn4/Projectn4/vector.c
@@ -0,0 +1,131 @@
+﻿#include "vector.h"
+
+void allocate(TVector* v, int n)
+{
+	v->n = n;
+	v->x = (double*)malloc(sizeof(double) * n);
+}
+
+void fill(TVector* v)
+{
+	printf("fillvector\n");
+
+	for (int i = 0; i < (v->n); i++) {
+		scanf("%lf", &(v->x[i]));
+	}
+}
+
+void print(TVector* v)
+{
+	int i = 0;
+	printf("Vector: ");
+	for (; i < v->n; i++)
+	{
+		printf("%.2lf", v->x[i]);
+	}
+	printf("\n");
+}
+
+TVector sum(TVector* v1, TVector* v2)
+{
+	TVector res;
+	if ((v1->n) != v2->n)
+	{
+		printf("(v1->n) != v2->n)");
+		res.x = NULL;
+		res.n = 0;
+		return res;
+
+	}
+	allocate(&res, v1->n);
+	for (int i = 0; i < res.n; i++)
+	{
+		res.x[i] = v1->x[i] + v2->x[i];
+	}
+	return res;
+}
+
+TVector sub(TVector* v1, TVector* v2)
+{
+	TVector res;
+	if ((v1->n) != v2->n)
+	{
+		printf("(v1->n) != v2->n)");
+		res.x = NULL;
+		res.n = 0;
+		return res;
+
+	}
+	allocate(&res, v1->n);
+	for (int i = 0; i < res.n; i++)
+	{
+		res.x[i] = v1->x[i] - v2->x[i];
+	}
+	return res;
+}
+
+TVector prod(TVector* v1, TVector* v2)
+{
+	TVector res;
+	if ((v1->n) != v2->n)
+	{
+		printf("(v1->n) != v2->n)");
+		res.x = NULL;
+		res.n = 0;
+		return res;
+
+	}
+	allocate(&res, 1);
+	res.x[0] = 0;
+	for (int i = 0; i < v1->n; i++)
+	{
+		res.x[0] += v1->x[i] * v2->x[i];
+	}
+	return res;
+}
+
+void read(const char* filename, TVector* v1, TVector* v2)
+{
+	int n1 = 0; int n2 = 0;
+
+	FILE* f = fopen(filename, "r+");
+	if (f == NULL)
+	{
+		printf("Файл не найден");
+		abort();
+	}
+	fscanf(f, "%d", &n1);
+	fscanf(f, "%d", &n2);
+	v1->n = n1;
+	v2->n = n2;
+	allocate(v1, n1);
+	allocate(v2, n2);
+	for (int i = 0; i < v1->n; i++) {
+		fscanf(f, "%lf", &(v1->x[i]));
+
+	}
+	for (int i = 0; i < v2->n; i++) {
+		fscanf(f, "%lf", &(v2->x[i]));
+
+	}
+	fclose(f);
+	return;
+}
+
+void write(const char* filename, TVector* res, char* s)
+{
+
+	FILE* f = fopen(filename, "a+");
+	if (f == NULL)
+	{
+		printf("Файл не найден");
+		abort();
+	}
+	fprintf(f, "%s", s);
+	for (int i = 0; i < res->n; i++)
+	{
+		fprintf(f, " %.2lf", res->x[i]);
+	}
+	fprintf(f, "\n");
+	fclose(f);
+}
